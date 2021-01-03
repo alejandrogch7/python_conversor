4e5f0b2f6ca9465be62d811a649838e5e609c741
Alejandro Gonzalez <68917510+alejandrogch7@users.noreply.github.com>
2021-01-02 19:45:07 -0500

    New currencies

--- conversor.py
+++ conversor.py
@@ -1,22 +1,48 @@
-divisa = input(" Introduce tu divisa, 1 para pesos Colombianos, 2 para dólares: ")
-divisa = float(divisa)
-divisa = round(divisa, 2)
+def transform_usd(currency, usd_value):
+    currency = float(input("Cúanto dinero tienes?"))
+    usd_value = currency/usd_value
+    usd_value = round(usd_value,2)
+    usd_value = str(usd_value)
+    print("Tienes $ "+ usd_value +" dólares")
 
-amount = input("introduce la cantidad a convertir: ")
-amount = float(amount)
+def usd_transform(currency, usd_value):
+    currency = float(input("Cúanto dinero tienes? "))
+    usd_value = currency*usd_value
+    usd_value = round(usd_value,2)
+    usd_value = str(usd_value)
+    print("Tienes $ "+ usd_value +" pesos")
 
-if divisa == 1: 
-    valor_dolar = 3700
-    tu_cambio = amount/valor_dolar
-    tu_cambio = round(tu_cambio, 2) 
-    tu_cambio = str(tu_cambio)
-    print("Tienes $"+ tu_cambio +" USD")
-else: 
-    valor_dolar = 3700 
-    tu_cambio = amount*valor_dolar
-    tu_cambio = round(tu_cambio, 2) 
-    tu_cambio = str(tu_cambio)
-    print("Tienes $"+ tu_cambio +" COP")
+menu = """
+Si vas a convertir de una divisa a dolares estas son tus opciones:  
+
+1 - Pesos Colombianos
+2 - Pesos Argentinos
+3 - Pesos Mexicanos
+
+Si vas a convertir tus dólares a una divisa, escoge cual, estas son tus opciones:
+
+4 - Pesos Colombianos
+5 - Pesos Argentinos 
+6 - Pesos Mexicanos
+
+Elige una opción: """
+
+option = int(input(menu))    
+
+if option == 1:
+    transform_usd("colombianos", 3422)
+elif option == 2:
+    transform_usd("argentinos", 85)
+elif option == 3:
+    transform_usd("mexicanos", 19)
+elif option == 4:
+    usd_transform("colombianos", 3422)
+elif option == 5:
+    usd_transform("argentinos", 85)
+elif option == 6:
+    usd_transform("mexicanos", 19)
+else:
+    print('Ingresa una opción correcta por favor')
 
 
     
